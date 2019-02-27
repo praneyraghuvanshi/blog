1c0c2e07b18e028bdedf4f0b915d8b00993a042c
praney <praney@fourtek.com>
2019-02-27 11:47:29 +0530

    deploy

---
+++ service-worker.js
@@ -0,0 +1,60 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "404.html",
+    "revision": "c5be49f593f569c35728247791f33225"
+  },
+  {
+    "url": "assets/css/0.styles.1566f855.css",
+    "revision": "0a7d97ea56ad8f6e62d1d6ad005c379d"
+  },
+  {
+    "url": "assets/img/search.83621669.svg",
+    "revision": "83621669651b9a3d4bf64d1a670ad856"
+  },
+  {
+    "url": "assets/js/2.0f4038f2.js",
+    "revision": "ac3c07dd1687b332f8d66d63c115fd6c"
+  },
+  {
+    "url": "assets/js/3.cd2edc0b.js",
+    "revision": "1ebb4f12a1b92d4a07af46671f2f2aee"
+  },
+  {
+    "url": "assets/js/app.b1f33482.js",
+    "revision": "53117e5f909d1c1efb71438517690859"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+addEventListener('message', event => {
+  const replyPort = event.ports[0]
+  const message = event.data
+  if (replyPort && message && message.type === 'skip-waiting') {
+    event.waitUntil(
+      self.skipWaiting().then(
+        () => replyPort.postMessage({ error: null }),
+        error => replyPort.postMessage({ error })
+      )
+    )
+  }
+})
